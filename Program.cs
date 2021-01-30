2bea4f497485b87a03936cc8f054ab43f75ad769
Dilruba-00 <68999015+Dilruba-00@users.noreply.github.com>
2021-01-30 21:57:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya elmasi";
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Diyarbakir karpuzu";
+
+            Product[] products = new Product[] {product1 ,product2 };
+
+            //type-safe -- tip güvenligi
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("------------------------");
+            }
+
+            Console.WriteLine("------------Methods------------");
+
+            //instance - örnek
+            //encapsulation
+
+            SepetMenager sepetManager = new SepetMenager();
+            sepetManager.Ekle(product1);
+            sepetManager.Ekle(product2);
+
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12 , 10);
+            sepetManager.Ekle2("Elma", "Yesil elma", 12, 3);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 14);
+
+        }
+    }
+}
+
+
---
+++ SepetMenager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetMenager
+    {
+        //nameing convention
+        //syntax
+        public void Ekle(Product product)//parantez varsa metod çalışıyor
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: "+ product.Adi);
+
+        }
+
+        public void Ekle2(string productAdi, string aciklama, double fiyat, int StokAdedi)//parantez varsa metod çalışıyor
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + productAdi);
+
+        }
+    }
+}
